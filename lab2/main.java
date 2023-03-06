7711c4ad0c561910ab149c8381bc7508f460757c
Adam Wasiela <43621020+AddyDouble@users.noreply.github.com>
2023-03-06 16:47:37 +0100

    Add swapcase for params

--- lab2/main.java
+++ lab2/main.java
@@ -5,7 +5,8 @@ class lab0 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
 			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-				System.out.print(param);
+				String nParam = swapcase(param);
+				System.out.print(nParam);
 				i++;
 			}else{
 				System.out.print(format_string.charAt(i));
@@ -22,4 +23,20 @@ class lab0 {
 			my_printf(format_string,param);
 		}
 	}
+
+	public static String swapcase(String text){
+        String newText = "";
+        for(int i = 0; i < text.length(); i++){
+            if(Character.isUpperCase(text.charAt(i))){
+                newText = newText + Character.toLowerCase(text.charAt(i));
+            }
+            else if(Character.isLowerCase(text.charAt(i))){
+                newText = newText + Character.toUpperCase(text.charAt(i));
+            }
+            else{
+                newText = newText + text.charAt(i);
+            }
+        }
+        return newText;
+    }
 }
