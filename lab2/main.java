f67b390b4fc267e3e9b02cf669d49a2512972861
Adam Wasiela <43621020+AddyDouble@users.noreply.github.com>
2023-03-06 16:40:23 +0100

    Add main program

---
+++ lab2/main.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+
+class lab0 {
+	public static void my_printf(String format_string, String param){
+		for(int i=0;i<format_string.length();i++){
+			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
+				System.out.print(param);
+				i++;
+			}else{
+				System.out.print(format_string.charAt(i));
+			}
+		}
+		System.out.println("");
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
+		String format_string, param;
+		while(bufferReader.ready()) {
+			format_string=bufferReader.readLine();
+			param=bufferReader.readLine();
+			my_printf(format_string,param);
+		}
+	}
+}
