e829d908e36343a0efd25758566fd7615b9c6d7b
Adam Wasiela <43621020+AddyDouble@users.noreply.github.com>
2023-03-06 17:28:53 +0100

    Add limiting string length

--- lab2/main.java
+++ lab2/main.java
@@ -1,14 +1,23 @@
 
 import java.io.*;
 
-class lab0 {
+class lab2 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
 			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
 				String nParam = swapcase(param);
 				System.out.print(nParam);
 				i++;
-			}else{
+			}else if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == '.') && i + 3 < format_string.length()){
+				int result = trim(format_string.substring(i+2), param);
+                if(result != -1){
+                    i += result + 2;
+                }
+                else{
+                    System.out.print(format_string.charAt(i));
+                }
+			}
+			else{
 				System.out.print(format_string.charAt(i));
 			}
 		}
@@ -38,5 +47,31 @@ class lab0 {
             }
         }
         return newText;
+    }
+
+	public static int trim(String text, String r){
+        int i = 0;
+        for(; i < text.length(); i++){
+            if(Character.isDigit(text.charAt(i))){
+                continue;
+            }
+            else if(text.charAt(i) == 'k'){
+                break;
+            }
+            else{
+                return -1;
+            }
+        }
+        try {
+            int val = Integer.parseInt(text.substring(0, i));
+            String newText = "";
+            for(int k = 0; k < val && k < r.length(); k++)
+                newText = newText + r.charAt(k);
+            System.out.print(swapcase(newText));
+            return i;
+        }
+        catch (Exception e){
+            return -1;
+        }
     }
 }
