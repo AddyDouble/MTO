339869fefb3e7f2b83e968ac269c6703d0bccf0c
Adam Wasiela <43621020+AddyDouble@users.noreply.github.com>
2023-03-13 16:53:23 +0100

    Add string padding for format #k

--- lab3/main.java
+++ lab3/main.java
@@ -2,27 +2,6 @@
 import java.io.*;
 
 class lab3 {
-	public static void my_printf(String format_string, String param){
-		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-				String nParam = swapcase(param);
-				System.out.print(nParam);
-				i++;
-			}else if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == '.') && i + 3 < format_string.length()){
-				int result = trim(format_string.substring(i+2), param);
-                if(result != -1){
-                    i += result + 2;
-                }
-                else{
-                    System.out.print(format_string.charAt(i));
-                }
-			}
-			else{
-				System.out.print(format_string.charAt(i));
-			}
-		}
-		System.out.println("");
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
 		String format_string, param;
@@ -33,7 +12,98 @@ class lab3 {
 		}
 	}
 
-	public static String swapcase(String text){
+	public static void my_printf(String format_string, String param){
+        for(int i=0;i<format_string.length();i++){
+            if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
+                String nParam = swapcase(param);
+                System.out.print(nParam);
+                i++;
+            }else if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == '.') && i + 3 < format_string.length()){
+                int result = trim(format_string.substring(i+2), param);
+                if(result != -1){
+                    i += result + 2;
+                }
+                else{
+                    System.out.print(format_string.charAt(i));
+                }
+            }
+            else if((format_string.charAt(i) == '#') && Character.isDigit(format_string.charAt(i+1)) && i + 2 < format_string.length()){
+                int result = padding(format_string.substring(i+1), param);
+                if(result != -1)
+                    i += result + 1;
+                else
+                    System.out.print(format_string.charAt(i));
+            }
+            else{
+                System.out.print(format_string.charAt(i));
+            }
+        }
+        System.out.println("");
+    }
+
+    public static int padding(String text, String arg){
+        int padLen = 0;
+        int len = 0;
+        boolean hasDot = false;
+        for(; len < text.length(); len++){
+            if(Character.isDigit(text.charAt(len))){
+                if(!hasDot)
+                    padLen++;
+                continue;
+            }
+            else if(text.charAt(len) == 'k'){
+                break;
+            }
+            else if(text.charAt(len) == '.'){
+                hasDot = true;
+                arg = trimArg(text.substring(len+1), arg);
+                if(arg == null)
+                    return -1;
+            }
+            else{
+                return -1;
+            }
+        }
+        try {
+            int val = Integer.parseInt(text.substring(0, padLen));
+            String newText = "";
+            int toFill = val - arg.length();
+            for(int k = 0; k < toFill; k++)
+                newText = newText + " ";
+            newText = newText + arg;
+            System.out.print(swapcase(newText));
+            return len;
+        }
+        catch (Exception e){
+            return -1;
+        }
+    }
+
+    public static String trimArg(String text, String arg){
+        int i = 0;
+        for(; i < text.length(); i++){
+            if(Character.isDigit(text.charAt(i))){
+                continue;
+            }
+            else if(text.charAt(i) == 'k'){
+                break;
+            }
+            else{
+                return null;
+            }
+        }
+        try {
+            int val = Integer.parseInt(text.substring(0, i));
+            String newText = "";
+            for(int k = 0; k < val && k < arg.length(); k++)
+                newText = newText + arg.charAt(k);
+            return newText;
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
+    public static String swapcase(String text){
         String newText = "";
         for(int i = 0; i < text.length(); i++){
             if(Character.isUpperCase(text.charAt(i))){
@@ -49,7 +119,7 @@ class lab3 {
         return newText;
     }
 
-	public static int trim(String text, String r){
+    public static int trim(String text, String r){
         int i = 0;
         for(; i < text.length(); i++){
             if(Character.isDigit(text.charAt(i))){
@@ -74,6 +144,8 @@ class lab3 {
             return -1;
         }
     }
+
+	
 }
 
 
