5cf376f25362575938753bcc5da30ca22b2044b4
Adam Wasiela <43621020+AddyDouble@users.noreply.github.com>
2023-05-29 17:45:06 +0200

    Add implementation for #a

--- lab10/main.java
+++ lab10/main.java
@@ -4,8 +4,8 @@ import java.io.*;
 class lab10 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-				System.out.print(param);
+			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'a')){
+				printNum(param);
 				i++;
 			}else{
 				System.out.print(format_string.charAt(i));
@@ -22,6 +22,24 @@ class lab10 {
 			format_string=bufferReader.readLine();
 			param=bufferReader.readLine();
 			my_printf(format_string,param);
+		}
+	}
+
+	private static void printNum(String param){
+		int N = param.length();
+		if(param.charAt(0) == '-')
+			N -= 1;
+
+		int O = Integer.parseInt(param);
+
+		int F = (int) (O*2)/N;
+
+		if(F % 2 == 0){
+			System.out.print(F);
+		}
+		else{
+			String hexa = String.format("%x", F);
+			System.out.print(hexa);
 		}
 	}
 }
